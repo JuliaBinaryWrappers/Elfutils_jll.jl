b9a3fb95ccce5233ed1bd9e177f7df9834723cdd
jlbuild <juliabuildbot@gmail.com>
2026-09-04 13:35:32 +0000

    Elfutils_jll build 0.196.0+0

--- src/Elfutils_jll.jl
+++ src/Elfutils_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Elfutils")
-JLLWrappers.@generate_main_file("Elfutils", UUID("ab5a07f8-06af-567f-a878-e8bb879eba5a"))
+JLLWrappers.@generate_main_file("Elfutils", Base.UUID("ab5a07f8-06af-567f-a878-e8bb879eba5a"))
 end  # module Elfutils_jll
